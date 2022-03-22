d74d5c86c42c201cab05fd593e1672e323a9ff94
aarnipex <anniina.p.e.aarnio@student.jyu.fi>
2022-03-22 10:52:34 +0200

    vaihe 6 tehtavat tiedostoluku ja -tallennus

--- src/kotitalous/Tehtavat.java
+++ src/kotitalous/Tehtavat.java
@@ -1,9 +1,15 @@
 package kotitalous;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Tehtävät-luokka:
@@ -78,6 +84,80 @@ public class Tehtavat implements Iterable<Tehtava> {
     }
     
     
+    /**
+     * Lukee tehtävät tiedostosta.
+     * @param hakemisto tiedoston nimen alkuosa
+     * @throws SailoException jos lukeminen epäonnistuu
+     * 
+     * @example
+     * <pre name="test">
+     * #THROWS SailoException
+     * #import java.io.File;
+     * #import java.util.Iterator;
+     *  Tehtavat teet = new Tehtavat();
+     *  Tehtava imu12 = new Tehtava(); imu12.rekisteroi(); imu12.taytaImurointiTiedoilla();
+     *  Tehtava imu23 = new Tehtava(); imu23.rekisteroi(); imu23.taytaImurointiTiedoilla();
+     *  Tehtava imu34 = new Tehtava(); imu34.rekisteroi(); imu34.taytaImurointiTiedoilla();
+     *  String tiedNimi = "testiKotitalous";
+     *  File ftied = new File(tiedNimi + "/tehtavat.dat");
+     *  ftied.delete();
+     *  teet.lueTiedostosta(tiedNimi); #THROWS SailoException
+     *  teet.lisaa(imu12);
+     *  teet.lisaa(imu23);
+     *  teet.lisaa(imu34);
+     *  teet.tallenna(tiedNimi);
+     *  teet = new Tehtavat();
+     *  teet.lueTiedostosta(tiedNimi);
+     *  Iterator<Tehtava> i = teet.iterator();
+     *  i.next().toString() === imu12.toString();
+     *  i.next().toString() === imu23.toString();
+     *  i.next().toString() === imu34.toString();
+     *  i.hasNext() === false;
+     *  teet.lisaa(imu12);
+     *  teet.tallenna(tiedNimi);
+     *  ftied.delete() === true; 
+     * </pre>
+     */
+    public void lueTiedostosta(String hakemisto) throws SailoException {
+        String nimi = hakemisto + "/tehtavat.dat";
+        File ftied = new File(nimi);
+        
+        try (Scanner fi = new Scanner(new FileInputStream(ftied))) {
+            while (fi.hasNext()) {
+                String s = fi.nextLine().trim();
+                if ("".equals(s) || s.charAt(0) == ';') continue;
+                Tehtava teht = new Tehtava();
+                teht.parse(s);                      // voisi kaivata virhekäsittelyä..
+                lisaa(teht);
+            }
+        }catch (FileNotFoundException e) {
+            throw new SailoException("Ei saa luettua tiedostoa " + nimi);
+        }
+    }
+    
+    
+    /**
+     * Tallentaa tehtävät tiedostoon.
+     * Tiedoston muoto:
+     * <pre>
+     * 1|Imurointi|10|30
+     * 3|Roskien vienti|3|7
+     * </pre>
+     * @param hakemisto tallennettavan tiedoston hakemisto
+     * @throws SailoException jos talletus epäonnistuu
+     */
+    public void tallenna(String hakemisto) throws SailoException {
+        File ftied = new File(hakemisto + "/tehtavat.dat");
+        try (PrintStream fo = new PrintStream(new FileOutputStream(ftied, false))) {
+            for (var teht : this.alkiot) {
+                fo.println(teht.toString());
+            }
+        } catch (FileNotFoundException ex) {
+            throw new SailoException("Tiedosto " + ftied.getAbsolutePath() + " ei aukea");
+        }
+    }
+    
+    
     @Override
     public Iterator<Tehtava> iterator() {
         return this.alkiot.iterator();
@@ -89,6 +169,13 @@ public class Tehtavat implements Iterable<Tehtava> {
      */
     public static void main(String[] args) {
         Tehtavat teet = new Tehtavat();
+        
+        try {
+            teet.lueTiedostosta("kotitalous");
+        } catch (SailoException ex) {
+            System.err.println(ex.getMessage());
+        }
+        
         Tehtava imurointi1 = new Tehtava();
         Tehtava imurointi2 = new Tehtava();
         imurointi1.rekisteroi();
@@ -100,14 +187,18 @@ public class Tehtavat implements Iterable<Tehtava> {
         teet.lisaa(imurointi2);
         
         System.out.println("=================Tehtävä-testi===============");
-        try {
-            teet.etsi(1).tulosta(System.out);
-            teet.etsi(2).tulosta(System.out);
-            teet.etsi(3).tulosta(System.out);
 
-        } catch (SailoException e) {
-            System.out.println(e.getMessage());
+        for (Tehtava t : teet.annaKaikki()) {
+            t.tulosta(System.out);
         }
+        
+        try {
+            teet.tallenna("kotitalous");
+        } catch (SailoException e) {
+            e.printStackTrace();
+        }
+        
+        
     }
 
 
